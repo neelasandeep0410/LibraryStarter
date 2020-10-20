12abb3debb4c54cea058d40e6625f171f891bb05
Jeffry Gonzalez <jeff@hypertheory.com>
2020-10-20 14:07:09 -0400

    reservations started

---
+++ LibraryApi/Controllers/ReservationsController.cs
@@ -0,0 +1,26 @@
+﻿using LibraryApi.Models.Reservations;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryApi.Controllers
+{
+    public class ReservationsController : ControllerBase
+    {
+
+        [HttpPost("reservations")]
+        public async Task<ActionResult> AddReservation([FromBody] PostReservationRequest request)
+        {
+            return Ok();
+        }
+
+
+        [HttpGet("reservations/{id}")]
+        public async Task<ActionResult> GetReservationById(int id)
+        {
+            return Ok();
+        }
+    }
+}
